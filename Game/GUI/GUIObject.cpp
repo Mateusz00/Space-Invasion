7d39bf407f5dcbfc50b189840d7c5423fa7610f7
Mateusz00 <skmateusz226@gmail.com>
2018-10-21 16:43:58 +0200

    Added abstract class that will be used as base for all GUI objects

---
+++ Game/GUI/GUIObject.cpp
@@ -0,0 +1,41 @@
+#include "GUIObject.hpp"
+
+GUIObject::GUIObject()
+    : mIsSelected(false),
+      mIsActive(false)
+{
+}
+
+GUIObject::~GUIObject()
+{
+}
+
+void GUIObject::select()
+{
+    mIsSelected = true;
+}
+
+void GUIObject::deselect()
+{
+    mIsSelected = false;
+}
+
+bool GUIObject::isSelected()
+{
+    return mIsSelected;
+}
+
+bool GUIObject::isActive() const
+{
+    return mIsActive;
+}
+
+void GUIObject::activate()
+{
+    mIsActive = true;
+}
+
+void GUIObject::deactivate()
+{
+    mIsActive = false;
+}
---
+++ Game/GUI/GUIObject.hpp
@@ -0,0 +1,27 @@
+#ifndef GUIOBJECT_HPP
+#define GUIOBJECT_HPP
+
+#include <SFML/Graphics/Drawable.hpp>
+#include <SFML/Graphics/Transformable.hpp>
+#include <SFML/Window/Event.hpp>
+
+class GUIObject : public sf::Drawable, public sf::Transformable
+{
+    public:
+                        GUIObject();
+        virtual         ~GUIObject();
+        virtual void    select();
+        virtual void    deselect();
+        virtual bool    isSelectable() const = 0;
+        bool            isSelected();
+        virtual bool    isActive() const;
+        virtual void	activate();
+        virtual void	deactivate();
+        virtual void	handleEvent(const sf::Event& event) = 0;
+
+    private:
+        bool mIsSelected;
+        bool mIsActive;
+};
+
+#endif // GUIOBJECT_HPP
