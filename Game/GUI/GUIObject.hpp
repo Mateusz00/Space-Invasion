40fddb52cc1b507ad2983c91eb877633f1cd0d87
Mateusz00 <skmateusz226@gmail.com>
2018-10-22 17:07:48 +0200

    Added function for checking collision of GUIObject with mouse

--- Game/GUI/GUIObject.hpp
+++ Game/GUI/GUIObject.hpp
@@ -4,20 +4,25 @@
 #include <SFML/Graphics/Drawable.hpp>
 #include <SFML/Graphics/Transformable.hpp>
 #include <SFML/Window/Event.hpp>
+#include <SFML/Graphics/Rect.hpp>
+#include <memory>
 
 class GUIObject : public sf::Drawable, public sf::Transformable
 {
     public:
-                        GUIObject();
-        virtual         ~GUIObject();
-        virtual void    select();
-        virtual void    deselect();
-        virtual bool    isSelectable() const = 0;
-        bool            isSelected();
-        virtual bool    isActive() const;
-        virtual void	activate();
-        virtual void	deactivate();
-        virtual void	handleEvent(const sf::Event& event) = 0;
+        using ComponentPtr = std::unique_ptr<GUIObject>;
+
+                                GUIObject();
+        virtual                 ~GUIObject();
+        virtual void            select();
+        virtual void            deselect();
+        virtual bool            isSelectable() const = 0;
+        bool                    isSelected();
+        virtual bool            isActive() const;
+        virtual void	        activate();
+        virtual void	        deactivate();
+        virtual void	        handleEvent(const sf::Event& event) = 0;
+        virtual sf::FloatRect   getBoundingRect() const = 0;
 
     private:
         bool mIsSelected;
