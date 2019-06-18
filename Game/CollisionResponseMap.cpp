57cf0ebcd0343ad98e1ae8cda064bb172d613fcf
Mateusz00 <skmateusz226@gmail.com>
2019-06-18 22:53:20 +0200

    Added collision response managing class

---
+++ Game/CollisionResponseMap.cpp
@@ -0,0 +1,35 @@
+#include "CollisionResponseMap.hpp"
+#include "Entities/Entity.hpp"
+
+void CollisionResponseMap::addResponse(Category::Type type1, Category::Type type2, Response response)
+{
+    (mCollisionResponsesMap[type1])[type2] = response;
+}
+
+void CollisionResponseMap::useResponse(Entity* entity1, Entity* entity2) const
+{
+    Category::Type type1 = entity1->getCategories();
+    Category::Type type2 = entity2->getCategories();
+
+    auto foundPos = mCollisionResponsesMap.find(type1);
+    Category::Type searchType;
+
+    if(foundPos != mCollisionResponsesMap.end())
+    {
+        searchType = type2;
+    }
+    else if((foundPos = mCollisionResponsesMap.find(type2)) != mCollisionResponsesMap.end())
+    {
+        searchType = type1;
+        std::swap(entity1, entity2);
+    }
+    else
+    {
+        return;
+    }
+
+    auto responsePos = foundPos->second.find(searchType);
+
+    if(responsePos != foundPos->second.end())
+        (responsePos->second)(*entity1, *entity2);
+}
---
+++ Game/CollisionResponseMap.hpp
@@ -0,0 +1,48 @@
+#ifndef COLLISIONRESPONSEMAP_HPP
+#define COLLISIONRESPONSEMAP_HPP
+
+#include "Category.hpp"
+#include <unordered_map>
+#include <functional>
+class Entity;
+
+class CollisionResponseMap
+{
+    public:
+        using Response = std::function<void(Entity&, Entity&)>;
+        using ResponseMap = std::unordered_map<Category::Type, Response, std::hash<int>>;
+
+        void    addResponse(Category::Type, Category::Type, Response);
+        void    useResponse(Entity*, Entity*) const;
+
+    private:
+        std::unordered_map<Category::Type, ResponseMap, std::hash<int>> mCollisionResponsesMap;
+};
+
+template <typename Object, typename Function>
+std::function<void(Entity&, Entity&)> castResponse(Function f)
+{
+    return [=](Entity& entity1, Entity& entity2)
+    {
+        if(dynamic_cast<Object*>(&entity1) == nullptr)
+            throw std::logic_error("Invalid cast!");
+
+        f(static_cast<Object&>(entity1), entity2);
+    };
+}
+
+template <typename Object1, typename Object2, typename Function>
+std::function<void(Entity&, Entity&)> castResponse(Function f)
+{
+    return [=](Entity& entity1, Entity& entity2)
+    {
+        if(dynamic_cast<Object1*>(&entity1) == nullptr)
+            throw std::logic_error("Invalid cast!");
+
+        if(dynamic_cast<Object2*>(&entity2) == nullptr)
+            throw std::logic_error("Invalid cast!");
+
+        f(static_cast<Object1&>(entity1), static_cast<Object2&>(entity2));
+    };
+}
+#endif // COLLISIONRESPONSEMAP_HPP
